28581f572b67d80ca778a2188ad594ba204da4ce
Rahul Bavaliya <41228277+RahulBavaliya@users.noreply.github.com>
2025-10-08 11:27:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   base: process.env.VERCEL ? '/' : 'Aro-Care-Customer-Service',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
